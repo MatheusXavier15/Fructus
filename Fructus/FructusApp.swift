e3762aed12a40206b6b33745ded413b49c6a769d
Matheus Xavier <matheus.oliveira@rhitmo.com.br>
2023-07-29 21:18:54 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Matheus Xavier on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
